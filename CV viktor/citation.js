2a260e7801601217ee432528949c24d1aba871fc
Morkchoklad <49789750+Morkchoklad@users.noreply.github.com>
2022-07-29 12:17:02 -0400

    Add files via upload

---
+++ CV viktor/citation.js
@@ -0,0 +1,90 @@
+let c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20;
+    c1 = "« Avant, la techmaturgie n'était qu'un outil. C'est désormais une libération. »"
+    c2 = "« La glorieuse évolution est en marche. »"
+    c3 = "« Abandonnez la chair. »"
+    c4 = "« La fonction domine la forme. »"
+    c5 = "« Ma conception est supérieure. »"
+    c6 = "« Le métal c'est la perfection. »"
+    c7 = "« On va vous déconnecter pour mise à jour. »"
+    c8 = "« L'acier corrige l'imperfection. »"
+    c9 = "« D'autres vont me rejoindre. »"
+    c10 = "« Adaptez-vous ou disparaissez. »"
+    c11 = "« La vraie puissance ! »"
+    c12 = "« Voyez ! »"
+    c13 = "« Oblitération ! »"
+    c14 = "« Désintégration ! »"
+    c15 = "« Manufacture au rabais. »"
+    c16 = "« Ils sont obsolètes. »"
+    c17 = "« Leurs corps sont fragiles. »"
+    c18 = "« Acceptez le progrès ! »"
+    c19 = "« Tout va bientôt changer. »"
+    c20 = "« Parfaitement efficace. »"
+
+function nouvcit() {
+    switch (Math.ceil(Math.random()*20)) {
+        case 1:
+            document.getElementById("citation").innerHTML = c1;
+        break;
+        case 2:
+            document.getElementById("citation").innerHTML = c2;
+        break;
+        case 3:
+            document.getElementById("citation").innerHTML = c3;
+        break;
+        case 4:
+            document.getElementById("citation").innerHTML = c4;
+        break;
+        case 5:
+            document.getElementById("citation").innerHTML = c5;
+        break;
+        case 6:
+            document.getElementById("citation").innerHTML = c6;
+        break;
+        case 7:
+            document.getElementById("citation").innerHTML = c7;
+        break;
+        case 8:
+            document.getElementById("citation").innerHTML = c8;
+        break;
+        case 9:
+            document.getElementById("citation").innerHTML = c9;
+        break;
+        case 10:
+            document.getElementById("citation").innerHTML = c10;
+        break;
+        case 11:
+            document.getElementById("citation").innerHTML = c11;
+        break;
+        case 12:
+            document.getElementById("citation").innerHTML = c12;
+        break;
+        case 13:
+            document.getElementById("citation").innerHTML = c13;
+        break;
+        case 14:
+            document.getElementById("citation").innerHTML = c14;
+        break;
+        case 15:
+            document.getElementById("citation").innerHTML = c15;
+        break;
+        case 16:
+            document.getElementById("citation").innerHTML = c16;
+        break;
+        case 17:
+            document.getElementById("citation").innerHTML = c17;
+        break;
+        case 18:
+            document.getElementById("citation").innerHTML = c18;
+        break;
+        case 19:
+            document.getElementById("citation").innerHTML = c19;
+        break;
+        case 20:
+            document.getElementById("citation").innerHTML = c20;
+        break;
+    }
+}
+
+
+
+
